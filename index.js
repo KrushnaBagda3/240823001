3dff0b1938f22915ff9f86757e378e4114b706da
Krushna Bagda <krushnabagda03@gmail.com>
2025-07-15 13:14:08 +0530

    node

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log("hello world")
+
+// function checkOddEven(number) {
+//     if (number % 2 === 0) {
+//       return "Even";
+//     } else {
+//       return "Odd";
+//     }
+//   }
+  
+//   console.log(checkOddEven(4)); 
+//   console.log(checkOddEven(7)); 
+
+const numbers = [10, 5, 20, 8, 15];
+const maxNumber = Math.max(...numbers); 
+console.log(maxNumber); 
