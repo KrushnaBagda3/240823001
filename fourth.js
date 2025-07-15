c7eed3e5c099964d330314d4944f17c0e2d251ae
Krushna Bagda <krushnabagda03@gmail.com>
2025-07-15 13:34:54 +0530

    node

---
+++ fourth.js
@@ -0,0 +1,8 @@
+function sum_of_natural_numbers(n) {
+    return (n * (n + 1)) / 2;
+  }
+  
+  // Example usage
+  let n = 5;
+  let sum = sum_of_natural_numbers(n);
+  console.log("Sum of the first", n, "natural numbers:", sum); // Output: Sum of the first 5 natural numbers: 15
